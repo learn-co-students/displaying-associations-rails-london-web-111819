1138b4a4b984b821abaceafd1c8d45f8cb0d50ad
github username <github email address>
2019-12-17 12:56:14 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,20 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   Mayor.create(name: 'Emanuel', city: cities.fir
+8.times do
+    Category.create(:name => Faker::Book.genre)
+end
+
+8.times do 
+    Author.create(:name => Faker::Name.name,
+                 :genre => Faker::Book.genre,
+                 :bio => Faker::Hipster.sentence)
+end
+
+8.times do 
+    Post.create(:title => Faker::Book.title,
+                :description => Faker::Hipster.sentence,
+                :author_id => Author.all.sample.id,
+                :category_id => Category.all.sample.id)
+end
